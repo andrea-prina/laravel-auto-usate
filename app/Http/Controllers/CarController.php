d520932138616ade76c2057765a233de1987fd18
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-12 17:05:51 +0200

    create controller

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    public function(){
+        return view('homepage');
+    }
+}
