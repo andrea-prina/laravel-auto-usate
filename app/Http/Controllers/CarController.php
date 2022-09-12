428f0bddfe4cfef514eafab35e60721451245101
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-12 17:14:06 +0200

    controller update model

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Car;
 use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
     public function(){
+
+        $cars = Car::all();
+
         return view('homepage');
     }
 }
